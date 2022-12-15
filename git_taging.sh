209800f02b90f1dc6d007138aee720079a338e8d
Enzo <esx0004088_prosegur>
2022-12-15 14:55:54 +0100

    Update git_taging.sh

--- git_taging.sh
+++ git_taging.sh
@@ -2,6 +2,9 @@
 #get highest tag number
 VERSION=`git describe --abbrev=0 --tags` 
  echo "VERSION" $VERSION 
+
+
+
 #replace . with space so can split into an array
 VERSION_BITS=(${VERSION//./ })
  echo "VERSION_BITS" $VERSION_BITS
@@ -15,23 +18,29 @@ VNUM1=`echo $VNUM1 | sed 's/v//'`
 MAJOR=`git log --format=%B -n 1 HEAD | grep '#major'`
 MINOR=`git log --format=%B -n 1 HEAD | grep '#minor'`
 
-if [ "$MAJOR" ]; then
-    echo "Update major version"
-    VNUM1=$((VNUM1+1))
-    VNUM2=0
-    VNUM3=0
-elif [ "$MINOR" ]; then
-    echo "Update minor version"
-    VNUM2=$((VNUM2+1))
-    VNUM3=0
-else
-    echo "Update patch version"
-    VNUM3=$((VNUM3+1))
+ if [ -z "$VERSION" ]
+then
+      echo "\$VERSION is empty" 
+      NEW_TAG="1.0.0"
+else 
+    if [ "$MAJOR" ]; then
+        echo "Update major version"
+        VNUM1=$((VNUM1+1))
+        VNUM2=0
+        VNUM3=0
+    elif [ "$MINOR" ]; then
+        echo "Update minor version"
+        VNUM2=$((VNUM2+1))
+        VNUM3=0
+    else
+        echo "Update patch version"
+        VNUM3=$((VNUM3+1))
+    fi
+    #create new tag
+    NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 fi
 
 
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 
 echo "Updating $VERSION to $NEW_TAG"
 
