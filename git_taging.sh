0596da81d3392206e7caee89f68e5cf57d9680e2
Enzo <esx0004088_prosegur>
2022-12-16 08:59:12 +0100

    #minor

--- git_taging.sh
+++ git_taging.sh
@@ -1,8 +1,8 @@
 #!/bin/bashmajor_max=1;
 #get highest tag number
 VERSION=`git describe --abbrev=0 --tags` 
- echo "VERSION" $VERSION 
-echo $1
+echo "VERSION" $VERSION 
+
 echo "comment 1: " $1
 
 #replace . with space so can split into an array
@@ -21,7 +21,7 @@ MINOR=`git log --format=%B -n 1 HEAD | grep '#minor'`
  if [ -z "$VERSION" ]
 then
       echo "\$VERSION is empty" 
-      NEW_TAG="1.0.0"
+      #NEW_TAG="1.0.0"
 else 
     if [ "$MAJOR" ]; then
         echo "Update major version"
@@ -38,23 +38,23 @@ else
     fi
     #create new tag
     NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+    echo "Updating $VERSION to $NEW_TAG"
+
+    #get current hash and see if it already has a tag
+    GIT_COMMIT=`git rev-parse HEAD`
+    NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+
+    #only tag if no tag already (would be better if the git describe command above could have a silent option)
+    if [ -z "$NEEDS_TAG" ]; then
+        git config --global user.email "enzo.cordova@hotmail.com"
+        git config --global user.name "enzocordova"
+        echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+        git tag -a $NEW_TAG -m "$1"
+        git push --tags
+    else
+        echo "Already a tag on this commit"
+    fi 
 fi
 
 
 
-echo "Updating $VERSION to $NEW_TAG"
-
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT`
-
-#only tag if no tag already (would be better if the git describe command above could have a silent option)
-if [ -z "$NEEDS_TAG" ]; then
-    git config --global user.email "enzo.cordova@hotmail.com"
-    git config --global user.name "enzocordova"
-    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-    git tag -a $NEW_TAG -m "$1"
-    git push --tags
-else
-    echo "Already a tag on this commit"
-fi 
